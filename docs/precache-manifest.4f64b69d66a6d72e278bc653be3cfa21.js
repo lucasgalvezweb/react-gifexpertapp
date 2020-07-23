3331865293002899788cd72b64644167e73b69e2
Lucas Galvez <lucasgalch@gmail.com>
2020-07-22 23:31:48 -0500

    Docs agregado

---
+++ docs/precache-manifest.4f64b69d66a6d72e278bc653be3cfa21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b5ade2c33ab55bb50d4426e9b956540",
+    "url": "/index.html"
+  },
+  {
+    "revision": "055475612981869ea4cf",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "055475612981869ea4cf",
+    "url": "/static/js/main.5d5c5250.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
